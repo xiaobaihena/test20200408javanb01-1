3db463bdd7c51c690f31eb24a0eaba9331c375f4
xiaobaihena <2507246051@qq.com>
2020-04-08 21:23:34 +0800

    小王：222

--- src/com/rj/bd/user/action/UserAction.java
+++ src/com/rj/bd/user/action/UserAction.java
@@ -2,11 +2,11 @@ package com.rj.bd.user.action;
 /**
  * @desc   
  * @author baixinping
- * @time   2020年4月8日下午7:23:07
+ * @time   2020骞�4鏈�8鏃ヤ笅鍗�7:23:07
  */
 public class UserAction {
 
 	public void SayHi(){
-		
+		System.out.println("小王:222");
 	}
 }
